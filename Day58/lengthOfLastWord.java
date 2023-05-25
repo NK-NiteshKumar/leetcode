256ce238f3da190a57c82318eb49da9369d4189e
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2023-05-25 20:55:31 +0530

    Create lengthOfLastWord.java

---
+++ Day58/lengthOfLastWord.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        boolean nik=false;
+        int ans=0;
+        for(int i=s.length()-1; i>=0; i--){
+            if(s.charAt(i)==' '){
+                if(nik){
+                    break;
+                }
+            }
+            else{
+                nik=true;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
+
+//https://leetcode.com/problems/length-of-last-word/
