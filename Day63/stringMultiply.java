1000d85c5ad5776a68a6481c818c09b464b1a26f
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2023-05-30 21:18:40 +0530

    Create stringMultiply.java

---
+++ Day63/stringMultiply.java
@@ -0,0 +1,7 @@
+class Solution {
+    public static String multiply(String num1, String num2) {
+        return String.valueOf((new java.math.BigInteger(num1)).multiply(new java.math.BigInteger(num2)));
+    }
+}
+
+//https://leetcode.com/problems/multiply-strings/
