71404415bed34813b1a8f5efc5101d220b84e69c
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2023-06-02 21:15:32 +0530

    Create inorderPreorder.java

---
+++ Day66/inorderPreorder.java
@@ -0,0 +1,19 @@
+class Solution {
+   int i, p; 
+   public TreeNode buildTree(int[] pre, int[] in) {
+      i = p = 0;
+      return dfs(pre, in, 3001);
+   }
+
+   private TreeNode dfs(int[] pre, int[] in, int rightBoundary) {
+      if (p == pre.length || in[i] == rightBoundary) return null;
+
+      TreeNode node = new TreeNode(pre[p++]);
+      node.left  = dfs(pre, in, node.val);
+      i++;
+      node.right = dfs(pre, in, rightBoundary);
+      return node;
+   }
+}
+
+//https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
