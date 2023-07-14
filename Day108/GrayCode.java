81017cdc3b6d949fa0eb84c1acf70add0c02865d
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2023-07-14 22:08:17 +0530

    Create GrayCode.java

---
+++ Day108/GrayCode.java
@@ -0,0 +1,17 @@
+class GrayCode {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = new ArrayList<>();
+        res.add(0);
+        for (int i = 1; i <= n; i++) {
+            int count = res.size() - 1;
+            int add = (int) Math.pow(2, i - 1);
+            while (count >= 0) {
+                res.add(add + res.get(count));
+                count--;
+            }
+        }
+        return res;
+    }
+}
+
+//https://leetcode.com/problems/gray-code/
