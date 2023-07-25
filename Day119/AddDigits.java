78298a102a919f3cfbd0e11a3930f6eb319964d2
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2023-07-25 22:13:33 +0530

    Create AddDigits.java

---
+++ Day119/AddDigits.java
@@ -0,0 +1,17 @@
+class AddDigits {
+    public int addDigits(int num) {
+        while(num>9)num=check(num);
+        return num;
+    }
+
+    public int check(int num){
+        int n=0;
+        while(num!=0){
+            n+=num%10;
+            num/=10;
+        }
+        return n;
+    }
+}
+
+//https://leetcode.com/problems/add-digits/
