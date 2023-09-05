c8c8481f65e10f82e5764cabccf72e77afd4c303
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2023-09-05 22:24:39 +0530

    Create CopyRandomList.java

---
+++ Day161/CopyRandomList.java
@@ -0,0 +1,24 @@
+public class CopyRandomList {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+        
+        HashMap<Node, Node> oldToNew = new HashMap<>();
+        
+        Node curr = head;
+        while (curr != null) {
+            oldToNew.put(curr, new Node(curr.val));
+            curr = curr.next;
+        }
+        
+        curr = head;
+        while (curr != null) {
+            oldToNew.get(curr).next = oldToNew.get(curr.next);
+            oldToNew.get(curr).random = oldToNew.get(curr.random);
+            curr = curr.next;
+        }
+        
+        return oldToNew.get(head);
+    }
+}
+
+//https://leetcode.com/problems/copy-list-with-random-pointer/
