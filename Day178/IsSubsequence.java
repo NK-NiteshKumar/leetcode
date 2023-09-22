ade7a47644879a0c355e73f23ec6b2b16bb85274
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2023-09-22 22:28:42 +0530

    Create IsSubsequence.java

---
+++ Day178/IsSubsequence.java
@@ -0,0 +1,21 @@
+class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int slen= s.length();
+        int tlen = t.length();
+        int j=0;
+        if(slen==0)
+            return true;
+        if(tlen==0)
+            return false;
+        for(int i=0;i<tlen;i++)
+        {
+            if(s.charAt(j)==t.charAt(i))
+                j++;
+            if(j==slen)
+                return true;
+        }
+        return false;
+    }
+}
+
+//https://leetcode.com/problems/is-subsequence/
