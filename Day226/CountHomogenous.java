9dc35e719653cca565b6d703fce0649dac8f5e63
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2023-11-09 19:08:00 +0530

    Create CountHomogenous.java

---
+++ Day226/CountHomogenous.java
@@ -0,0 +1,27 @@
+class CountHomogenous {
+    public int countHomogenous(String s) {
+        long res  = 0;
+        char[] c = s.toCharArray();
+        int start =0;
+        for(int i=0;i<c.length;i++){
+            if(c[i]!=c[start]){
+                int appear = i-start;
+                while(appear>0){
+                    res+=appear;
+                    appear-=1;
+                }
+                start=i;
+            }
+        }
+
+        int appear = c.length-start;
+        while(appear>0){
+            res+=appear;
+            appear-=1;
+        }
+
+        return (int)(res%(Math.pow(10,9)+7));
+    }
+}
+
+//https://leetcode.com/problems/count-number-of-homogenous-substrings/
