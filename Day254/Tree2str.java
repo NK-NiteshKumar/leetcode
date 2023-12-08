fec6a22d2f61ef7c976240abd6bc7a9979880192
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2023-12-08 21:41:51 +0530

    Create Tree2str.java

---
+++ Day254/Tree2str.java
@@ -0,0 +1,25 @@
+class Tree2str {
+    public String tree2str(TreeNode t) {
+        StringBuilder res = new StringBuilder();
+        dfs(t, res);
+        return res.toString();
+    }
+
+    public static void dfs(TreeNode t, StringBuilder res) {
+        if (t == null)
+            return;
+        res.append(String.valueOf(t.val));
+        if (t.left == null && t.right == null)
+            return;
+        res.append('(');
+        dfs(t.left, res);
+        res.append(')');
+        if (t.right != null) {
+            res.append('(');
+            dfs(t.right, res);
+            res.append(')');
+        }
+    }
+}
+
+//https://leetcode.com/problems/construct-string-from-binary-tree/
