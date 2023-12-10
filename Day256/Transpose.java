46b0c6689b1d6a321ffe4b6fb68005421df04515
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2023-12-10 22:21:54 +0530

    Create Transpose.java

---
+++ Day256/Transpose.java
@@ -0,0 +1,17 @@
+class Transpose {
+    public int[][] transpose(int[][] matrix) {
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int result[][]=new int[col][row];
+        for(int i=0;i<col;i++)
+        {
+            for(int j=0;j<row;j++)
+            {
+            result[i][j]=matrix[j][i];
+            }
+        }
+        return result;
+    }
+}
+
+//https://leetcode.com/problems/transpose-matrix/
