4f003207320f11d74965e1f37385c4b6ee7e2ded
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2023-12-27 21:44:16 +0530

    Create MinCost.java

---
+++ Day273/MinCost.java
@@ -0,0 +1,19 @@
+class MinCost {
+    public int minCost(String colors, int[] neededTime) {
+        int needed=0;
+        int n=neededTime.length;
+        char[] chc=colors.toCharArray();
+        int cur_max=neededTime[0];
+        for(int i=1;i<n;i++){
+            if(chc[i-1]!=chc[i]){
+                cur_max=neededTime[i];
+            }else{
+                needed+=Math.min(cur_max, neededTime[i]);
+                cur_max=Math.max(cur_max, neededTime[i]);
+            }
+        }
+        return needed;
+    }
+}
+
+//https://leetcode.com/problems/minimum-time-to-make-rope-colorful/
