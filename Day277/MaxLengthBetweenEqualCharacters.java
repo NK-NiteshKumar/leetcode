7daa1fcb445bc28836ae902337a63fb84f0fb564
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2023-12-31 20:54:26 +0530

    Create MaxLengthBetweenEqualCharacters.java

---
+++ Day277/MaxLengthBetweenEqualCharacters.java
@@ -0,0 +1,18 @@
+class MaxLengthBetweenEqualCharacters {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        Map<Character, Integer> firstIndex = new HashMap();
+        int ans = -1;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (firstIndex.containsKey(s.charAt(i))) {
+                ans = Math.max(ans, i - firstIndex.get(s.charAt(i)) - 1);
+            } else {
+                firstIndex.put(s.charAt(i), i);
+            }
+        }
+        
+        return ans;
+    }
+}
+
+//https://leetcode.com/problems/largest-substring-between-two-equal-characters/description/
