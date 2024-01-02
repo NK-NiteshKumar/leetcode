0d847a86a4fac597159d6066fd4ef75ffd31fe3f
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-01-02 21:16:55 +0530

    Create FindMatrix.java

---
+++ Day279/FindMatrix.java
@@ -0,0 +1,19 @@
+class FindMatrix {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        int[] freq = new int[nums.length + 1];
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int c : nums) {
+            if (freq[c] >= ans.size()) {
+                ans.add(new ArrayList<>());
+            }
+
+            ans.get(freq[c]).add(c);
+            freq[c]++;
+        }
+
+        return ans;
+    }
+}
+
+//https://leetcode.com/problems/convert-an-array-into-a-2d-array-with-conditions/
