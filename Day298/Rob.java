5c3c9f0bdc281d0f7a766a58c858e45791adec03
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-01-21 23:28:51 +0530

    Create Rob.java

---
+++ Day298/Rob.java
@@ -0,0 +1,15 @@
+class Rob {
+    public int rob(int[] nums) {
+        int rob = 0;
+        int norob = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            int newRob = norob + nums[i];
+            int newNoRob = Math.max(norob, rob);
+            rob = newRob;
+            norob = newNoRob;
+        }
+        return Math.max(rob, norob);
+    }
+}
+
+//https://leetcode.com/problems/house-robber/
