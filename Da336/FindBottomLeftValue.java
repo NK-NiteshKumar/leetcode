b0ff31f6178cb84d7bc0c443e007d360dff2098c
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-02-28 21:28:00 +0530

    Create FindBottomLeftValue.java

---
+++ Da336/FindBottomLeftValue.java
@@ -0,0 +1,23 @@
+public class FindBottomLeftValue {
+    public int findBottomLeftValue(TreeNode root) {
+        int last = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int count = q.size();
+            for (int i = 0; i < count; i++) {
+                TreeNode curr = q.poll();
+                if (i == 0)
+                    last = curr.val;  // last leftMost val
+                if (curr.left != null)
+                    q.add(curr.left);
+                if (curr.right != null)
+                    q.add(curr.right);
+            }
+        }
+        return last;
+    }
+}
+
+//https://leetcode.com/problems/find-bottom-left-tree-value/
