9230fdf61a9334bc825d6abf6d3f5379727e192c
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-03-02 22:46:23 +0530

    Create SortedSquares.java

---
+++ Day339/SortedSquares.java
@@ -0,0 +1,11 @@
+class SortedSquares {
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
+
+//https://leetcode.com/problems/squares-of-a-sorted-array/
