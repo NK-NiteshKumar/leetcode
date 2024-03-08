d99167c3b887a19c3a83742900c9ddf6f04570e0
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-03-08 21:12:19 +0530

    Create MaxFrequencyElements.java

---
+++ Day345/MaxFrequencyElements.java
@@ -0,0 +1,14 @@
+class MaxFrequencyElements {
+    public int maxFrequencyElements(int[] nums) {
+        int ans = 0;
+        int maxFreq = Integer.MIN_VALUE;
+        int[] count = new int[101];
+
+        for(int num : nums) count[num]++;
+        for(int num : count)    maxFreq = Math.max(maxFreq, num);        
+        for(int num : nums) if(maxFreq == count[num])   ans++;
+        return ans;
+    }
+}
+
+//https://leetcode.com/problems/count-elements-with-maximum-frequency/
