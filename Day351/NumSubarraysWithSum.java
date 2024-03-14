6758e117d51dd08a0f66423381b0ac44bb3ae433
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-03-14 22:21:33 +0530

    Create NumSubarraysWithSum.java

---
+++ Day351/NumSubarraysWithSum.java
@@ -0,0 +1,17 @@
+class NumSubarraysWithSum {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int n = nums.length;
+        int result = 0;
+        int prefixSum = 0;
+        Map<Integer,Integer> map = new HashMap();
+        map.put(0,1);
+        for(int i=0; i<n; i++){
+            prefixSum += nums[i];
+            result += map.getOrDefault(prefixSum - goal, 0);
+            map.put(prefixSum, map.getOrDefault(prefixSum,0) + 1);
+        }
+        return result;
+    }
+}
+
+//https://leetcode.com/problems/binary-subarrays-with-sum/
