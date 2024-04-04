b01a9628c8cc553a9a9a9533622af3587f3ceca3
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-04-04 21:25:22 +0530

    Create MaxDepth.java

---
+++ Day372/MaxDepth.java
@@ -0,0 +1,20 @@
+class MaxDepth {
+    public int maxDepth(String s) {
+
+        int ans = 0;
+        int countOpen = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                countOpen++;
+            } else if (s.charAt(i) == ')') {
+                countOpen--;
+            }
+            ans = Math.max(ans, countOpen);
+        }
+        
+        return ans;
+    }
+}
+
+//https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
