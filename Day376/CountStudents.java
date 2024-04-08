719ecf3f0da458890fc868f5fde4507bd795d306
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-04-08 21:41:34 +0530

    Create CountStudents.java

---
+++ Day376/CountStudents.java
@@ -0,0 +1,35 @@
+class CountStudents {
+    public int countStudents(int[] students, int[] sandwiches) {
+        
+        int circleCount = 0;
+        int squareCount = 0;
+
+        for(int stud : students) {
+            if(stud == 0) {
+                circleCount++;
+            }
+            else {
+                squareCount++;
+            }
+        }
+
+        for(int sandwich : sandwiches) {
+
+            if(sandwich == 0 && circleCount == 0) {
+                return squareCount;
+            }
+            if(sandwich == 1 && squareCount == 0) {
+                return circleCount;
+            }
+            if(sandwich == 0) {
+                circleCount--;
+            }
+            else {
+                squareCount--;
+            }
+        }
+        return 0;
+    }
+}
+
+//https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/
