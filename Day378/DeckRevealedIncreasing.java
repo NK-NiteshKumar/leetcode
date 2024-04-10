1ecff4c1aa58f65a2ae1247bb75d5098193c2d56
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-04-10 22:07:03 +0530

    Create DeckRevealedIncreasing.java

---
+++ Day378/DeckRevealedIncreasing.java
@@ -0,0 +1,25 @@
+class DeckRevealedIncreasing {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        
+        int n = deck.length;
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 0; i < n; i++) {
+            queue.offer(i);
+        }
+
+        Arrays.sort(deck);
+
+        int ans[] = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            ans[queue.poll()] = deck[i];
+
+            queue.offer(queue.poll());
+        }
+        return ans;
+    }
+}
+
+//https://leetcode.com/problems/reveal-cards-in-increasing-order/
