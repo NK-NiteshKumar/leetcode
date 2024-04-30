8b511901cb7f0784d35709606cb5834d87340bfa
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-04-30 21:04:04 +0530

    Create WonderfulSubstrings.java

---
+++ Day398/WonderfulSubstrings.java
@@ -0,0 +1,21 @@
+class WonderfulSubstrings {
+    public long wonderfulSubstrings(String word) {
+        
+        int count[] = new int[1024];
+        int mask = 0;
+        count[0] = 1;
+
+        long ans = 0;
+        for(char c : word.toCharArray()) {
+            mask ^= 1 << (c - 'a');
+            ans += count[mask];
+            for(int i = 0; i < 10; i++) {
+                ans += count[mask^(1 << i)];
+            }
+            count[mask]++;
+        }
+        return ans;
+    }
+}
+
+//https://leetcode.com/problems/number-of-wonderful-substrings/
