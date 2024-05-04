97ca744ca53867098af9d768a68290a79f5b12af
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-05-04 22:19:31 +0530

    Create NumRescueBoats.java

---
+++ date/20240504/NumRescueBoats.java
@@ -0,0 +1,38 @@
+class NumRescueBoats {
+    public int numRescueBoats(int[] people, int limit) {
+        
+        int boatCount = 0;
+
+        int[] count = new int[limit + 1];
+        for (int p : people) {
+            count[p]++;
+        }
+
+        int idx = 0;
+        for (int val = 1; val <= limit; val++) {
+            while (count[val]-- > 0) {
+                people[idx++] = val;
+            }
+        }
+
+        int left = 0;
+        int right = people.length - 1;
+
+        while(left <= right) {
+            int sum = people[left] + people[right];
+
+            if(sum <= limit) {
+                boatCount++;
+                left++;
+                right--;
+            }
+            else {
+                boatCount++;
+                right--;
+            }
+        }
+        return boatCount;
+    }
+}
+
+//https://leetcode.com/problems/boats-to-save-people/
