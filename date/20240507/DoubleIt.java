8e4bd29bf7eccd46f8414cd42e184b8a49c076b5
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-05-07 22:29:00 +0530

    Create DoubleIt.java

---
+++ date/20240507/DoubleIt.java
@@ -0,0 +1,23 @@
+public class DoubleIt {
+    public int twiceOfVal(ListNode head) {
+        if (head == null) return 0;
+        
+        int doubledValue = head.val * 2 + twiceOfVal(head.next);
+        
+        head.val = doubledValue % 10;
+        
+        return doubledValue / 10;
+    }
+    
+    public ListNode doubleIt(ListNode head) {
+        int carry = twiceOfVal(head);
+        
+        if (carry != 0) {
+            head = new ListNode(carry, head);
+        }
+        
+        return head;
+    }
+}
+
+//https://leetcode.com/problems/double-a-number-represented-as-a-linked-list/
