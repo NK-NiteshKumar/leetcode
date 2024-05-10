8761c2ecb4763d30ddc13dcc574d9fdb010481f4
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-05-10 21:10:43 +0530

    Create KthSmallestPrimeFraction.java

---
+++ date/20240510/KthSmallestPrimeFraction.java
@@ -0,0 +1,23 @@
+class KthSmallestPrimeFraction {
+    public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+        
+        int n = arr.length;
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                double div = (double) arr[i] / arr[j];
+                pq.offer(new double[]{div, (double) arr[i], (double) arr[j]});
+
+                if (pq.size() > k) {
+                    pq.poll();
+                }
+            }
+        }
+
+        double[] ans = pq.poll();
+        return new int[]{(int) ans[1], (int) ans[2]};
+    }
+}
+
+//https://leetcode.com/problems/k-th-smallest-prime-fraction/
