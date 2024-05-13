6d86b4a452ed5e414f98df3de48cf825dde14538
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-05-13 21:44:45 +0530

    Create MatrixScore.java

---
+++ date/20240513/MatrixScore.java
@@ -0,0 +1,22 @@
+class MatrixScore {
+    public int matrixScore(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int ans = n * (1 << (m - 1));
+
+        for (int j = 1; j < m; j++) {
+            int colCount = 0;
+            for (int i = 0; i < n; i++) 
+                colCount += grid[i][0] == 0 ? (grid[i][j] ^ 1) : grid[i][j];
+
+            if (colCount < n - colCount) {
+                ans += (n - colCount) * (1 << (m - 1 - j));
+            } else {
+                ans += (colCount) * (1 << (m - 1 - j));
+            }
+        }
+        return ans;
+    }
+}
+
+//https://leetcode.com/problems/score-after-flipping-matrix/
