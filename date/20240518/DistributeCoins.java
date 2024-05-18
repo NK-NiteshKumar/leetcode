3384a491e2bcbb0626dd10f6abc88400d5f2b536
Nitesh Kumar <104667965+NK-NiteshKumar@users.noreply.github.com>
2024-05-18 21:52:54 +0530

    Create DistributeCoins.java

---
+++ date/20240518/DistributeCoins.java
@@ -0,0 +1,21 @@
+class DistributeCoins {
+    private int steps = 0;
+
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftReq = dfs(node.left);
+        int rightReq = dfs(node.right);
+
+        steps += Math.abs(leftReq) + Math.abs(rightReq);
+
+        return (node.val - 1) + leftReq + rightReq;
+    }
+
+    public int distributeCoins(TreeNode root) {
+        dfs(root);
+        return steps;
+    }
+}
+
+//https://leetcode.com/problems/distribute-coins-in-binary-tree/
